e35a4388b37e39530003354cf7e4cecfcdef0063
nkalkstein <nkalkstein@gmail.com>
2018-02-21 09:32:04 -0500

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,24 @@
+class Dog
+
+  def name=(name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+
+  fido = Dog.new
+  
+
+  def breed=(breed)
+    @breed = breed
+  end
+
+  def breed
+    @breed
+  end
+
+
+
+end
---
+++ lib/person.rb
@@ -0,0 +1,24 @@
+class Person
+
+  def name=(name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+
+  beyonce = Dog.new
+
+
+  def job=(job)
+    @job = job
+  end
+
+  def job
+    @job
+  end
+
+
+
+end
